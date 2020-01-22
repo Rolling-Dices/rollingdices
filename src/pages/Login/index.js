9631c1be079429631925fe8266fcb8431ba0fff1
Eden Kiefer <edenkieferjunior0@gmail.com>
2020-01-22 12:36:48 -0300

    Página de Login e logo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,49 @@
 import React from 'react';
 
+import logo from './Logo.png'
+
 import './styles.css';
 
 export default class Login extends React.Component{
-  constructor() {
-
-  }
-
   render() {
     return (
       <div>
-        Login
+        <div id="logo">
+          <img src={logo} alt="logo"/>
+        </div>
+        <div id="login">
+          <form action="">
+            <div className="input-block">
+              <label htmlFor="email">E-mail</label>
+              <input
+                type="email"
+                name="email" 
+                id="email" 
+                required
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="password">Senha</label>
+              <input 
+                type="password"
+                name="password" 
+                id="password" 
+                required
+              />
+            </div>
+            <div className="input-group">
+              <input 
+                type="checkbox"
+                name="checkbox" 
+                id="checkbox" 
+              />
+              <label htmlFor="password">Lembrar senha ?</label>
+            </div>
+            <button type="submit">Entrar</button>
+            <label>Ou</label>
+            <button href="/login">Cadastre-se</button>
+          </form>
+        </div>
       </div>
     );
   }
