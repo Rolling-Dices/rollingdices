56735833b59e89b98375944d8a0f71f209319324
Gabriel Felix <gabriel.felix@wizecompany.com.br>
2020-02-03 14:58:51 -0300

    criando tela da mesa de jogo

--- src/pages/Table/index.js
+++ src/pages/Table/index.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import './styles.css';
+import Header from '../../components/Header';
+import { MdAccountCircle } from 'react-icons/md';
+import grid from './grid.jpg';
 
-export default class index extends Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        );
-    }
+export default class Table extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="game-table">
+          <div id="table-tools">
+            <span><MdAccountCircle size={40} /></span>
+            <span><MdAccountCircle size={40} /></span>
+            <span><MdAccountCircle size={40} /></span>
+            <span><MdAccountCircle size={40} /></span>
+          </div>
+          <div id="table-map">
+            <img src={grid} alt="map" />
+          </div>
+          <div id="table-chat">
+            <input type="text" />
+          </div>
+          <button>Submit</button>
+        </div>
+      </>
+    );
+  }
 }
 
 
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import User from './pages/User';
+import Table from './pages/Table';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path='/login' component={Login} />
       <Route path='/user/:slug' component={User} />
+      <Route path='/table' component={Table} />
     </Switch>
   </BrowserRouter>
 );
